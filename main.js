368234a14cf89a922908c7cd17fdfeb144fd6683
Omanshi <90082608+OmanshiRajBanasya@users.noreply.github.com>
2021-09-28 14:54:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startClassiffication()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
